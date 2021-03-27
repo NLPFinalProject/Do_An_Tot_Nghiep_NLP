a49989ad8d999e9c6d1f92765ed14f5860aa066e
An <ghostkey.war@gmail.com>
2021-03-27 20:40:45 +0700

    Setting.py add MEDIA urls

--- PlagismDetector/PlagismDetector/PlagismDetector/settings.py
+++ PlagismDetector/PlagismDetector/PlagismDetector/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -155,3 +155,8 @@ EMAIL_HOST_USER = 'kaitouthuan@gmail.com'
 EMAIL_HOST_PASSWORD = 'kyonaruto'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+#vị trí lưu file trong thư mục media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'UserDocumentMedia')
+
+MEDIA_URL = '/UserDocumentMedia/'
