36a1ae9360138a8dee35fcafcf96233caddf7920
An <ghostkey.war@gmail.com>
2021-03-27 15:10:09 +0700

    change branch-3
    change branch-3 to match front-end branch

--- PlagismDetector/PlagismDetector/FileComponent/models.py
+++ PlagismDetector/PlagismDetector/FileComponent/models.py
@@ -1,38 +1,12 @@
 from django.db import models
-import datetime
-from django.utils import timezone
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-
-
-#---------model đồ án
 class DataDocument(models.Model):
-    DataDocumentName = models.CharField(max_length=200)
-    DataDocumentType = models.CharField(max_length=10)
-    DataDocumentAuthor = models.CharField(max_length=100)
-    def __str__(self):
-        return self.DataDocumentName
-    def AuthorName(self):
-        return self.DataDocumentAuthor
-    def DocumentType(self):
-        return self.DataDocumentType
-#file người dùng up post
-class DataDocumentT(models.Model):
     DataDocumentName = models.CharField(max_length=200)
     DataDocumentType = models.CharField(max_length=10)
     DataDocumentAuthor = models.CharField(max_length=100)
@@ -44,20 +18,6 @@ class DataDocumentT(models.Model):
     def DocumentType(self):
         return self.DataDocumentType
 # nội dung file được tách thành các câu
-class DataDocumentContentT(models.Model):
-    DataDocumentNo = models.ForeignKey(DataDocumentT, on_delete=models.CASCADE)
-    DataDocumentSentence = models.CharField(max_length=200)
-    DataDocumentSentenceLength = models.IntegerField(default=0)
-    def __str__(self):
-        return self.DataDocumentSentence
-    def DocName(self):
-        return self.DataDocumentNo
-    class Meta:
-        indexes = [
-            models.Index(fields=['DataDocumentNo','DataDocumentSentence'], name='DataDocumentNoT_idx'),
-            models.Index(fields=['DataDocumentSentence'], name='DataDocumentSentenceT_idx'),
-        ]
-
 class DataDocumentContent(models.Model):
     DataDocumentNo = models.ForeignKey(DataDocument, on_delete=models.CASCADE)
     DataDocumentSentence = models.CharField(max_length=200)
--- PlagismDetector/PlagismDetector/FileComponent/urls.py
+++ PlagismDetector/PlagismDetector/FileComponent/urls.py
@@ -1,26 +1,18 @@
+from django.contrib import admin
+from django.conf.urls import url, include 
 from django.urls import path
-#can import cho up file
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-
-app_name = 'polls'
+from FileComponent import views
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
-    # ex: ''
-    path('upload/', views.uploadDoc, name='upload'),
-    # ex: ''
-    path('', views.documentimport, name='demo'),
-
-    path('books/', views.book_list, name='book_list'),
-    # ex: /polls/
-    path('', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    url(r'^test2', views.documentimport),
+    url(r'^test', views.test),
+   
+    url(r'^uploadfile',csrf_exempt(views.uploadDoc)),
+    url(r'^uploadfilelist',csrf_exempt(views.uploadDoc)),
+    #url(r'^', include('UserComponent.urls')), 
+    #url(r'^$', views.home, name='home'),
+   # url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    #url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
+    #url(r'^admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
