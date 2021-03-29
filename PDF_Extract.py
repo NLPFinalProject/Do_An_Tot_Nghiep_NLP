85ef9a5e5ebf717efbb3db6cba9f662c5db27ec6
binhez <81172362+binhez@users.noreply.github.com>
2021-03-29 22:30:57 +0700

    Update PDF_Extract.py

--- PDF_Extract.py
+++ PDF_Extract.py
@@ -1,16 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-pip install pdfminer3
-
-
-# In[3]:
-
-
-from pdfminer3.layout import LAParams, LTTextBox
+rom pdfminer3.layout import LAParams, LTTextBox
 from pdfminer3.pdfpage import PDFPage
 from pdfminer3.pdfinterp import PDFResourceManager
 from pdfminer3.pdfinterp import PDFPageInterpreter
@@ -18,39 +6,47 @@ from pdfminer3.converter import PDFPageAggregator
 from pdfminer3.converter import TextConverter
 import io
 
-def pdf2text(path):
+
+def split_text(text):
+    list_para = text.split("\n\n")
+    for i in range(len(list_para)):
+        list_para[i] = list_para[i].replace('\n', '')
+    return list_para
+
+
+def pdf2txt_page(file_path):
+    list_page = []
+    with open(file_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            resource_manager = PDFResourceManager()
+            fake_file_handle = io.StringIO()
+            converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+            page_interpreter = PDFPageInterpreter(resource_manager, converter)
+            page_interpreter.process_page(page)
+            list_page.append(fake_file_handle.getvalue())
+            converter.close()
+            fake_file_handle.close()
+    list_para = []
+    for i in range(len(list_page)):
+        list_para.append(split_text(list_page[i]))
+    return list_para
+
+
+def pdf2txt_para(file_path):
     resource_manager = PDFResourceManager()
     fake_file_handle = io.StringIO()
     converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
     page_interpreter = PDFPageInterpreter(resource_manager, converter)
 
-    with open(path, 'rb') as fh:
-
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
+    list_page = []
+    with open(file_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
             page_interpreter.process_page(page)
-
+            print(fake_file_handle.getvalue())
         text = fake_file_handle.getvalue()
 
     # close open handles
     converter.close()
     fake_file_handle.close()
-
-    data = text.split("\n\n")
-
-    for i in range (0, len(data)):
-        data[i] = " ".join(data[i].split())
-    for i in range(0, len(data)):
-        data[i] = data[i].replace('-\n', '')
-        data[i] = data[i].replace('\n', ' ')
-    return data
-path = 'D:/MyDoc/Deep_learning/Yim_A_Gift_From_CVPR_2017_paper.pdf'
-pdf2text(path)
-
-
-# In[ ]:
-
-
-
-
+    list_para = split_text(text)
+    return list_para
