330f7d09c93412ef9f44579eb6fe5d6336a49d7b
diephaibinh <69073385+diephaibinh@users.noreply.github.com>
2021-03-31 01:09:27 +0700

    Update TF-IDF.py
    - Thêm hàm check() và preprocess_text_file()
    - Thay đổi cách tìm keyword

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,56 +1,77 @@
-from vncorenlp import VnCoreNLP
 from operator import itemgetter
 import math
 import string
+from Pdf_extract import read_pages
 
 
 # Cách chạy chương trình:
-# Sửa path của các file  và thực thi chương trình 
-# Chương trình đang trong quá trình hoàn thiện, còn rời rạc nên tạm thời chỉ có thể test bằng file txt
+# Sửa path của các file và thực thi chương trình 
 
 
-STOPWORD_FILE_PATH = './stopword.txt'
-TEXT_FILE_PATH = './test.txt'
-VNCORENLP_FILE_PATH = 'VnCoreNLP/VnCoreNLP-1.1.1.jar'
+DOC_FILE_PATH = 'test.pdf'
+STOPWORD_FILE_PATH = 'stopword.txt'
+ALPHABET_FILE_PATH = 'alphabet.txt'
 
 
 
-# Danh sách các stop word. VD: và, là, các, trong, ngoài, của,........
-stopword_file = open(STOPWORD_FILE_PATH, 'r', encoding='utf-8')
-stopwords = stopword_file.readlines()
-for i in range(len(stopwords)):
-        stopwords[i] = stopwords[i].replace('\n', '')
+# Tách dòng của text và lưu vào mảng
+# Input: file .txt
+# Output: danh sách các phần tử (list)
+def preprocess_text_file(txt_file):
+    f = open(txt_file, 'r', encoding='utf-8')
+    elements = f.readlines()
+    for i in range(len(elements)):
+        elements[i] = elements[i].replace('\n', '')
+    f.close()
 
-# Danh sách các dấu câu. VD ? ! . " " { } ( )
-punc = string.punctuation
-    
-stopword_file.close()
+    return elements
 
+
+
+# Kiểm tra trong 1 chuỗi có hợp lệ để sử dụng cho thuật toán TF-IDF hay không?
+# Ví dụ:
+#    + Chuỗi hợp lệ: Nguyễn_Ánh, đồng_minh, lãnh_thổ,...
+#    + Chuỗi không hợp lệ: 1972, Σ, σ(x),...
+# Input: chuỗi bất kỳ (string)
+# Output: 
+#    + True: hợp lệ
+#    + False: không hợp lệ  
+def check(string):
+    result = True
+    if len(string) == 1:
+        result = False
+    else:
+        for char in string:
+            if char not in alphabet:
+                result = False
+                break
+                
+    return result
 
 
 
 # Tìm tất cả từ phân biệt và tiền xử lý các từ
-# Input: list đã tách từ tách câu của vncore, list dấu câu (punctuation), stop word
+# Input: list đã tách từ tách câu của vncore, stop word
 # Output:  
 #    + Dictionary của tất cả từ phân biệt không bao gồm stop word, dấu câu với 
 #      key là từ A, value là số lượng từ A có trong văn bản (dict). VD: {hài_hước: 3, 'Bảo_Đại': 5} 
 #    + Tổng số từ của văn bản (int)
-def total_words_and_len(vncorenlp_tokenize, punctuation, stop_words):
+def total_words_and_len(vncorenlp_tokenize, stop_words):
     words = dict()
     doc_len = 0
 
     for s in vncorenlp_tokenize:
         for w in s:
             w = w.lower()
-            if w not in punctuation: 
-                doc_len += 1
-                if w not in stopwords:
-                    if w in words:
-                        words[w] += 1
-                    else:
-                        words[w] = 1
+            doc_len += 1
+            if w not in stopwords and check(w) == True:
+                if w in words:
+                    words[w] += 1
+                else:
+                    words[w] = 1
 
     return words, doc_len
+
 
 
 # Đếm số câu chứa từ cần check
@@ -65,12 +86,14 @@ def check_word_in_sent(word, vncorenlp_tokenize):
     return count
 
 
+
 # Lấy n giá trị cao nhất sắp xếp theo thứ tự giảm dần của một dictionary
 # Input: Dictionary có chứa giá trị kiểu số để có thể so sánh
 # Output: n giá trị cao nhất đã được sắp giảm dần
 def get_top(dic, n):
     result = dict(sorted(dic.items(), key = itemgetter(1), reverse = True)[:n]) 
     return result
+
 
 
 # Tính giá trị TF của tất cả từ
@@ -82,6 +105,7 @@ def TF(total_words_dict, doc_len):
         tf[key] = val / doc_len
     
     return tf
+
 
 
 # Tính giá trị IDF của tất cả từ
@@ -96,6 +120,7 @@ def IDF(total_words_dict, vncorenlp_tokenize):
     return idf
 
 
+
 # Tính giá trị TF-IDF của tất cả từ
 # Input: TF (dict) và IDF (dict)
 # Output: giá trị TF-IDF (dict)
@@ -107,16 +132,17 @@ def TFIDF(tf, idf):
 
 
 
-def Main():
-    vncorenlp_file = VNCORENLP_FILE_PATH
-    vncorenlp = VnCoreNLP(vncorenlp_file)
-    
-    f = open(TEXT_FILE_PATH, 'r', encoding='utf-8')
-    text = f.read()
-    f.close()
+# stopwords: Danh sách các stop word. VD: và, là, các, trong, ngoài, của,........
+# alphabet: các ký tự hợp lệ như: à, á, ư, b, j, đ, A, Ê, Z, ...... và '-'
+stopwords = preprocess_text_file(STOPWORD_FILE_PATH)
+alphabet = preprocess_text_file(ALPHABET_FILE_PATH)
 
-    tokenize = vncorenlp.tokenize(text)
-    words, len = total_words_and_len(tokenize, punc, stopwords)
+def Main():
+    start_page = 1
+    end_page = 3
+    tokenize = read_pages(start_page, end_page, DOC_FILE_PATH)
+
+    words, len = total_words_and_len(tokenize, stopwords)
 
     tf = TF(words, len)
     idf = IDF(words, tokenize)
